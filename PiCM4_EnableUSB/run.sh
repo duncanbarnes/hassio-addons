4ed54c99a32c34dba726c7761c03a796b949bf74
Duncan Barnes <github@duncanbarnes.net>
2023-12-15 18:20:11 +0000

    Debug

--- PiCM4_EnableUSB/run.sh
+++ PiCM4_EnableUSB/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 
-fun() {  while true; do nc -l -p 8099 -e  echo -e 'HTTP/1.1 200 OK\r\nServer: EnableCM4USB\r\nDate:$(date)\r\nContent-Type: text/html; charset=UTF8\r\nCache-Control: no-store, no cache, must-revalidate\r\n\r\n<!DOCTYPE html><html><body><p>Serial Ingress! It is here, and there is nothing left to say.</body></html>\r\n\n\n'; done }; fun&
+fun() {  while true; do nc -l -p 8099 -e  echo -e 'HTTP/1.1 200 OK\r\nServer: EnableCM4USB\r\nDate:$(date)\r\nContent-Type: text/html; charset=UTF8\r\nCache-Control: no-store, no cache, must-revalidate\r\n\r\n<!DOCTYPE html><html><body><p>CM4 USB Enable! It is here, and there is nothing left to say.</body></html>\r\n\n\n'; done }; fun&
 
 config='dtoverlay=dwc2,dr_mode=host'
 until false; do
@@ -15,17 +15,13 @@ set +e
   fi;
   echo "Listing root"
   ls -la /
-  echo "Listing boot"
-  ls -la /boot
+  echo "Listing dev"
+  ls -la /dev
 
   performWork () {
     partition=$1
     mount /dev/$partition /tmp/$partition 2>/dev/null
 	echo "==============================================="
-	echo "Contents of partition $partition"
-	ls -la /tmp/$partition
-	echo "Status of mount"
-	mount
     if [ -e /tmp/$partition/config.txt ]; then
       if [ $(grep "$config" /tmp/$partition/config.txt|grep -v \#) ]; then
         echo "USB already enabled on $partition";
